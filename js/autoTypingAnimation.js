baa8b2502413a85b67cb20adafe27521c9a7ef8c
kumarroshan12 <115878489+kumarroshan12@users.noreply.github.com>
2023-11-18 19:22:28 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Roshan Kumar Chaudhary</span> a 2nd year Bachelors in Technology CSE undergraduate at SRMIST. I am on a thrilling journey to explore the limitless world of technology and its applications.<br><br> With a keen interest in cutting-edge technologies, I am always on the lookout for new challenges that can push the boundaries of my knowledge. From programming languages like HTML,CSS and JavaScript to exploring the realms of artificial intelligence and machine learning, I am committed to staying at the forefront of technological advancements.While my primary focus is on technology, I believe in maintaining a holistic approach to life. Outside the world of programming, you might find me exploring the latest in sci-fi literature, participating in hackathons, or engaging in community service initiatives. "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
